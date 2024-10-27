2ec453e17e2595d33455876e1e704b2b46d1a7fc
ArcaneCipher <desjardins.jarp@gmail.com>
2024-10-26 23:40:15 -0400

    created fetchCoordsByIP function to get lat/long of specific IP. Used googleIPv4 as test IP

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss.js');
 
-fetchMyIP((error, ip) => {
+const googleIPv4 = "142.251.46.174";
+// const googleIPv6 = "2607:f8b0:4005:802::200e";
+
+/* fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
   console.log('It worked! Returned IP:' , ip);
+}); */
+
+fetchCoordsByIP(googleIPv4, (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
--- iss.js
+++ iss.js
@@ -26,8 +26,8 @@ const fetchMyIP = function (callback) {
 
     const ip = body.ip;
 
-    // Ensure the body has at least one result and the description exists
-    if (ip && ip.length > 0) {
+    // Ensure the body has at least one result
+    if (ip) {
       callback(null, ip); // Pass description to callback
     } else {
       callback("Failed to fetch IP", null); // Pass error message to callback
@@ -35,4 +35,45 @@ const fetchMyIP = function (callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+const fetchCoordsByIP = function (ip, callback) {
+  // use request to fetch IP address from JSON API
+  needle.get(`http://ipwho.is/${ip}`, (error, response, body) => {
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null); // Pass the error to the callback
+      return;
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    if (!body.success) {
+      const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    const latitude = body.latitude;
+    const longitude = body.longitude;
+    callback(null, {latitude, longitude});
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP,
+};
